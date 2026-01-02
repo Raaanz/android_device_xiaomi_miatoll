b98b4a4d81db19e809e1621c2a3118d8adc946ed
Raaanz <krithik.krishna.c@gmail.com>
2026-01-02 11:33:42 +0530

    Axion: adapt LineageOS trees for Axion

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -19,6 +19,21 @@ $(call inherit-product, device/xiaomi/miatoll/device.mk)
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Axion Stuff
+# Format: Main, Ultra-wide, Macro, Depth
+AXION_CAMERA_REAR_INFO := 64,8,5
+AXION_CAMERA_FRONT_INFO := 16
+AXION_PROCESSOR := Snapdragon_720G
+AXION_MAINTAINER := Raaanz
+TARGET_INCLUDES_LOS_PREBUILTS := true
+TARGET_ENABLE_BLUR := true
+
+# Axion CPU Flags (Snapdragon 720G uses a 6+2 configuration)
+# 6x Kryo 465 Silver (Efficiency) + 2x Kryo 465 Gold (Performance)
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES := 6,7
+
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
